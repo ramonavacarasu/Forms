8c8d722e988b3aece9d32e30ff0c717da7235992
ramonavacarasu <vacarasuramona@gmail.com>
2020-12-03 13:51:03 +0200

    ex-forms

---
+++ src/app/basic-template-form/basic-form-strict.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-basic-form-strict',
+  templateUrl: './basic-form-strict.component.html',
+})
+export class BasicFormStrictComponent {
+  users: Array<User>;
+  user: User;
+
+  constructor() {
+    this.users = new Array<User>();
+    this.user = new User(null, null);
+  }
+
+  onSave(f: NgForm) {
+    console.log('user', this.user);
+    console.log('valid?', f.valid);
+    if (f.valid) {
+      let newUser = new User(this.user.email, this.user.phone);
+      this.users.push(newUser);
+      f.reset();
+    }
+  }
+}
